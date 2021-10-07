2b3e2e451b2684fae12b37d8ef5557835af2e0af
Cieran O'Neill <cieran999@gmail.com>
2021-10-07 20:07:01 +0100

    Cleaned up "Command.cs" using two dictionaries instead of MASSIVE switch statement that was previously used due to lack of knowledge.

--- Command.cs
+++ Command.cs
@@ -1,71 +1,36 @@
 ﻿using static Stacker.Program;
 using static Stacker.Commands;
+using System.Collections.Generic;
 using System;
 
 namespace Stacker
 {
     class Command
     {
-        public enum COMMANDS 
+        public enum COMMANDS
         {
-            push, 
-            print, 
-            pop,
-            dup,
-            maths,
-            mem,
-            inc,
-            dec,
+            //COMMANDS
+            push, print, pop, dup, maths, mem, inc, dec,
             //BLOCKS
             loop
         }
 
+        static Dictionary<COMMANDS, Action<string[]>> commandDict = new Dictionary<COMMANDS, Action<string[]>>()
+        {
+            { COMMANDS.push, PUSH},{ COMMANDS.print, PRINT},{ COMMANDS.pop, POP},{ COMMANDS.dup, DUP},{ COMMANDS.maths, MATHS},{ COMMANDS.mem, MEM},{ COMMANDS.inc, INC},{ COMMANDS.dec, DEC},
+        };
+
+        static Dictionary<COMMANDS, Action<string[], Token[]>> blockDict = new Dictionary<COMMANDS, Action<string[], Token[]>>()
+        {
+            { COMMANDS.loop, LOOP}
+        };
+
         public COMMANDS INDEX { get; }
 
-        public Command(byte INDEX) 
-        {
-            this.INDEX = (COMMANDS)INDEX;
-        }
+        public Command(byte INDEX) => this.INDEX = (COMMANDS)INDEX;
 
-        public void Execute(string[] args) 
-        {
-                switch (INDEX)
-                {
-                    case COMMANDS.push:
-                        PUSH(args);
-                        break;
-                    case COMMANDS.print:
-                        PRINT(args);
-                        break;
-                    case COMMANDS.pop:
-                        POP(args);
-                        break;
-                    case COMMANDS.dup:
-                        DUP(args);
-                        break;
-                    case COMMANDS.maths:
-                        MATHS(args);
-                        break;
-                    case COMMANDS.mem:
-                        MEM(args);
-                        break;
-                    case COMMANDS.inc:
-                        INC(args);
-                        break;
-                    case COMMANDS.dec:
-                        DEC(args);
-                        break;
-                }
-        }
+        public void Execute(string[] args) => commandDict[INDEX].Invoke(args);
 
-        public void Execute(string[] args, Token[] tokens) 
-        {
-            switch (INDEX) 
-            { 
-                case COMMANDS.loop:
-                    LOOP(args, tokens);
-                    break;
-            }
-        }
+        public void Execute(string[] args, Token[] tokens) => blockDict[INDEX - commandDict.Count].Invoke(args, tokens);
     }
 }
